cc0b3705d383e884d2097da076127f02b424df71
Aftab Hussain <py-aftabhussain@mobiloitte.com>
2020-05-15 01:40:36 +0530

    view creation for patient/user all given review come on the basis of : -All the latest review based on the time will be shown -Get Api creation -response-latest view

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -31,73 +31,112 @@ class Home extends Component {
       isHealtCare: false,
     });
   }
+  ChangeColorFunction = () => {
+    var ColorCode =
+      'rgb(' +
+      Math.floor(Math.random() * 256) +
+      ',' +
+      Math.floor(Math.random() * 256) +
+      ',' +
+      Math.floor(Math.random() * 256) +
+      ')';
+
+    return ColorCode;
+  };
   groceydetails(item, index) {
     return (
       <View
         style={{
-          width: wp('85%'),
+          width: wp('90%'),
           alignSelf: 'center',
           height: hp('13%'),
           backgroundColor: 'white',
-          marginVertical: hp('1%'),
           flexDirection: 'row',
         }}>
-        <Image
-          resizeMode="contain"
-          source={{uri: item.image}}
-          style={{width: wp('25%'), height: hp('8%'), marginLeft: wp('3%')}}
-        />
         <View
           style={{
-            marginLeft: wp('2%'),
-            width: wp('55%'),
-            height: hp('12%'),
-            justifyContent: 'flex-end',
-            backgroundColor: 'white',
+            backgroundColor: this.ChangeColorFunction(),
+            width: 60,
+            height: 60,
+            borderRadius: 60 / 2,
+            marginTop: hp('2%'),
           }}>
           <Text
             style={{
-              marginTop: hp('2%'),
-              marginLeft: wp('2%'),
+              alignItems: 'center',
+              alignSelf: 'center',
+              marginTop: hp('1.2%'),
+              fontSize: 25,
               fontWeight: '700',
-              fontSize: 16,
+              justifyContent: 'center',
             }}>
-            {item.name}
+            {item.name[0]}
           </Text>
+        </View>
+        <View
+          style={{
+            marginLeft: wp('2%'),
+            width: wp('65%'),
+            height: hp('10%'),
+            backgroundColor: 'white',
+            flexDirection: 'row',
+            borderBottomColor: 'gray',
+            borderBottomWidth: 1,
+            paddingLeft: wp('-1%'),
+          }}>
           <View
             style={{
-              justifyContent: 'flex-end',
-              marginTop: hp('2%'),
-              flexDirection: 'row',
-              borderBottomWidth: 0.5,
+              flexDirection: 'column',
+              height: hp('10%'),
+              width: wp('70%'),
+              backgroundColor: 'white',
+              borderBottomColor: 'gray',
+              borderBottomWidth: 1,
             }}>
-            <TouchableOpacity>
-              <Image
-                style={{height: hp('4%'), width: wp('8%')}}
-                source={icon.whatsapp}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Image
+            <View style={{flexDirection: 'row'}}>
+              <Text
                 style={{
-                  height: hp('4%'),
-                  width: wp('8%'),
-                  marginLeft: wp('3%'),
-                }}
-                source={icon.call}
-              />
-            </TouchableOpacity>
-          </View>
-          <View>
-            <Text
-              style={{
-                marginBottom: hp('1.5%'),
-                marginLeft: wp('35%'),
-                fontSize: 16,
-                fontWeight: '700',
-              }}>
-              {item.area}
-            </Text>
+                  marginLeft: wp('2%'),
+                  marginTop: hp('1%'),
+                  fontWeight: '700',
+                  fontSize: 16,
+                }}>
+                {item.name}
+              </Text>
+            </View>
+            <View style={{flexDirection: 'row', marginTop: hp('2%')}}>
+              <Text
+                style={{
+                  marginTop: hp('1%'),
+                  marginLeft: wp('2%'),
+                  fontSize: 16,
+                  fontWeight: '700',
+                }}>
+                {item.area}
+              </Text>
+              <TouchableOpacity>
+                <Image
+                  style={{
+                    height: hp('4%'),
+                    width: wp('8%'),
+                    marginLeft: wp('32%'),
+                    marginTop: hp('-2%'),
+                  }}
+                  source={icon.whatsapp}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image
+                  style={{
+                    height: hp('4%'),
+                    width: wp('8%'),
+                    marginLeft: wp('4%'),
+                    marginTop: hp('-2%'),
+                  }}
+                  source={icon.call}
+                />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
@@ -105,7 +144,7 @@ class Home extends Component {
   }
   render() {
     return (
-      <View style={{backgroundColor: 'white'}}>
+      <View style={{backgroundColor: 'white', height: hp('100%')}}>
         <SafeAreaView>
           <View
             style={{
@@ -147,115 +186,111 @@ class Home extends Component {
               }}
             />
           </View>
-          <ScrollView>
-            <View
+          <View
+            style={{
+              height: hp('25%'),
+              width: wp('100%'),
+              backgroundColor: 'white',
+            }}>
+            <Image
+              resizeMode="contain"
+              source={icon.amour}
+              style={{width: wp('100%'), height: hp('24%')}}
+            />
+          </View>
+          <View
+            style={{
+              height: hp('6%'),
+              width: wp('100%'),
+              backgroundColor: 'white',
+              flexDirection: 'row',
+              alignItems: 'center',
+              alignSelf: 'flex-start',
+            }}>
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({
+                  itemList: all,
+                  isAll: true,
+                  isGrocry: false,
+                  isHealtCare: false,
+                });
+              }}
               style={{
-                height: hp('25%'),
-                width: wp('100%'),
-                backgroundColor: 'white',
-              }}>
-              <Image
-                resizeMode="contain"
-                source={icon.amour}
-                style={{width: wp('100%'), height: hp('25%')}}
-              />
-            </View>
-            <View
-              style={{
-                height: hp('6%'),
-                width: wp('100%'),
-                backgroundColor: 'white',
                 flexDirection: 'row',
+                justifyContent: 'space-between',
+                borderRadius: 79,
+                marginLeft: wp('2%'),
+                backgroundColor: this.state.isAll ? '#e5eeff' : 'white',
+                height: 35,
+                justifyContent: 'center',
                 alignItems: 'center',
-                alignSelf: 'center',
+                width: wp('18%'),
+                borderColor: 'black',
               }}>
-              <TouchableOpacity
-                onPress={() => {
-                  this.setState({
-                    itemList: all,
-                    isAll: true,
-                    isGrocry: false,
-                    isHealtCare: false,
-                  });
-                }}
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  borderRadius: 79,
-                  marginLeft: wp('2%'),
-                  backgroundColor: this.state.isAll ? '#e5eeff' : 'white',
-                  height: 35,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  width: wp('30%'),
-                  borderColor: 'black',
-                }}>
-                <Text style={{fontWeight: '500', fontSize: 20}}>All </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  this.setState({
-                    itemList: grocery,
-                    isAll: false,
-                    isGrocry: true,
-                    isHealtCare: false,
-                  });
-                }}
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  borderRadius: 79,
-                  marginLeft: wp('2%'),
-                  backgroundColor: this.state.isGrocry ? '#e5eeff' : 'white',
-                  height: 35,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  width: wp('30%'),
-                  borderColor: 'black',
-                }}>
-                <Text style={{fontWeight: '500', fontSize: 20}}>Grocery </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  this.setState({
-                    itemList: healtcare,
-                    isAll: false,
-                    isGrocry: false,
-                    isHealtCare: true,
-                  });
-                }}
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  borderRadius: 79,
-                  marginLeft: wp('2%'),
-                  backgroundColor: this.state.isHealtCare ? '#e5eeff' : 'white',
-                  height: 35,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  width: wp('30%'),
-                  borderColor: 'black',
-                }}>
-                <Text style={{fontWeight: '500', fontSize: 20}}>
-                  Healthcare{' '}
-                </Text>
-              </TouchableOpacity>
-            </View>
-            <View
+              <Text style={{fontWeight: '500', fontSize: 20}}>All </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({
+                  itemList: grocery,
+                  isAll: false,
+                  isGrocry: true,
+                  isHealtCare: false,
+                });
+              }}
               style={{
-                height: hp('65%'),
-                width: wp('100%'),
-                backgroundColor: 'white',
-                marginTop: hp('1%'),
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                borderRadius: 79,
+                marginLeft: wp('2%'),
+                backgroundColor: this.state.isGrocry ? '#e5eeff' : 'white',
+                height: 35,
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: wp('25%'),
+                borderColor: 'black',
               }}>
-              <FlatList
-                showsHorizontalScrollIndicator={false}
-                data={this.state.itemList}
-                renderItem={({item, index}) => this.groceydetails(item, index)}
-                keyExtractor={(item) => item.id}
-              />
-            </View>
-          </ScrollView>
+              <Text style={{fontWeight: '500', fontSize: 20}}>Grocery </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({
+                  itemList: healtcare,
+                  isAll: false,
+                  isGrocry: false,
+                  isHealtCare: true,
+                });
+              }}
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                borderRadius: 79,
+                marginLeft: wp('2%'),
+                backgroundColor: this.state.isHealtCare ? '#e5eeff' : 'white',
+                height: 35,
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: wp('30%'),
+                borderColor: 'black',
+              }}>
+              <Text style={{fontWeight: '500', fontSize: 20}}>Healthcare </Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              height: hp('100%'),
+              width: wp('100%'),
+              backgroundColor: 'white',
+              // marginTop: hp('1%'),
+            }}>
+            <FlatList
+              showsHorizontalScrollIndicator={false}
+              data={this.state.itemList}
+              renderItem={({item, index}) => this.groceydetails(item, index)}
+              keyExtractor={(item) => item.id}
+            />
+          </View>
         </SafeAreaView>
       </View>
     );
--- src/screens/Login/icons.js
+++ src/screens/Login/icons.js
@@ -2,11 +2,11 @@ const path = '../../assets/images/';
 const ext = 'jpeg';
 
 export default Icons = {
-  amour:require(`${path}amour.png`),
+  amour: require(`${path}amour.png`),
   whatsapp: require(`${path}whatsapp.png`),
   downarrow: require(`${path}downarrow.png`),
   loginIcon: require(`${path}icon.jpeg`),
-  call: require(`${path}call.jpg`),
+  call: require(`${path}call.png`),
 
   storenearme: require(`${path}storenearme.png`),
 };
